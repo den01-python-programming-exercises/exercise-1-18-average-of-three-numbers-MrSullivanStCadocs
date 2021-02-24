e582c31ff76a96de70559eb93d008838874b3c87
MrSullivan <rsullivan@stcadocs.com>
2021-02-24 23:15:15 +0000

    added three integer input variables and printed call back working out average.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    first_number = int(input("Give the first number:"))
+    second_number = int(input("Give the second number:"))
+    third_number = int(input("Give the third number:"))
+    print("The average is " + str((first_number+second_number+third_number)/3))
 
 if __name__ == '__main__':
     main()
